7d8bb25b94fdcaecd51fb5a3720e63caeed7fe6c
Kansuke Ikehara <ikeharakansuke@gmail.com>
2023-09-27 21:02:07 +0200

    update the url string for meetup.com

--- hamburg_events_scraping/meetup/meetup/spiders/hamburg_meetup.py
+++ hamburg_events_scraping/meetup/meetup/spiders/hamburg_meetup.py
@@ -3,7 +3,10 @@ import pendulum
 
 class HamburgMeetupSpider(scrapy.Spider):
     name = "hamburg_meetup"
-    BASE_URL = 'https://www.meetup.com/find/?suggested=true&source=EVENTS&location=de--Hamburg&eventType=inPerson&dateRange=today&distance=twentyFiveMiles&sortField=RELEVANCE'
+    TODAY = pendulum.now("Europe/Berlin").to_date_string()
+    DATE_RANGE = f'customStartDate={TODAY}T00%3A00%3A00-04%3A00&customEndDate={TODAY}T23%3A59%3A00-04%3A00'
+    BASE_URL = f'https://www.meetup.com/find/?suggested=true&source=EVENTS&location=de--Hamburg&eventType=inPerson&distance=twentyFiveMiles&sortField=RELEVANCE&{DATE_RANGE}'
+
     
     start_urls = [
         BASE_URL
@@ -12,7 +15,7 @@ class HamburgMeetupSpider(scrapy.Spider):
 
     def __init__(self, *args, **kwargs):
         super(HamburgMeetupSpider, self).__init__(*args, **kwargs)
-        self.custom_time = pendulum.now("Europe/Berlin").to_date_string()
+        self.custom_time =  self.TODAY
 
     def parse(self, response):
         for event in response.xpath('//div[@data-recommendationid]'):
